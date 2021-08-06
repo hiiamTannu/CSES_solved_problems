b931e2baab3493d974bcdee96252adc4ad4d0a03
hiiamTannu <84806426+hiiamTannu@users.noreply.github.com>
2021-08-06 18:19:41 +0530

    Update missing_number.cpp

--- missing_number.cpp
+++ missing_number.cpp
@@ -1,3 +1,31 @@
+/*
+You are given all numbers between 1,2,…,n except one. Your task is to find the missing number.
+
+Input
+
+The first input line contains an integer n.
+
+The second line contains n−1 numbers. Each number is distinct and between 1 and n (inclusive).
+
+Output
+
+Print the missing number.
+
+Constraints
+2≤n≤2⋅105
+Example
+
+Input:
+5
+2 3 1 5
+
+Output:
+4
+
+Problem Link:
+https://cses.fi/problemset/task/1083
+*/
+
 #include<iostream>
 using namespace std;
 int missingNo(long int arr[], long int n)
